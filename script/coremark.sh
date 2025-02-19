48c3b2db4a8d3e03938be54f021bbb261af6e0d5
ENJOU1224 <enjou1224@outlook.com>
2025-02-19 20:04:06 +0800

    改进了下coremark.sh

--- script/coremark.sh
+++ script/coremark.sh
@@ -1,6 +1,5 @@
+cd $T1_HOME
 # prepare the binary file
 git clone https://github.com/OpenXiangShan/ready-to-run.git
 # run the workload
-./build/RISCV/gem5.opt ./configs/example/xiangshan.py --raw-cpt --generic-rv-cpt=./ready-to-run/coremark-2-iteration.bin
-# get the ipc
-grep 'cpu.ipc' m5out/stats.txt
+$GEM5_HOME/build/RISCV/gem5.opt $GEM5_HOME/configs/example/xiangshan.py --raw-cpt --generic-rv-cpt=$T1_HOME/ready-to-run/coremark-2-iteration.bin
