01a0e211fa9a014d5e8d50f6c71ac2ea675fcfe3
ENJOU1224 <enjou1224@outlook.com>
2025-03-16 01:07:27 +0800

    简单增加注释说明script/docker.sh功能

--- script/docker.sh
+++ script/docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker compose down
-docker compose up -d
+docker compose down   # 停止并删除旧容器
+docker compose up -d  # 自动执行以下操作：
+                      # 1. 如果镜像不存在 → 根据 docker-compose.yml 中的 build 配置构建镜像
+                      # 2. 挂载文件卷
+                      # 3. 以守护进程模式启动容器
