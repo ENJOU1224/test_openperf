bc7fbb8a292dee9351aa0d6a1c1ee0b038abc6a1
ENJOU1224 <enjou1224@outlook.com>
2025-03-31 18:20:20 +0800

    修复gem5编译脚本不完全使用docker内环境编译以及路径不对的问题

--- script/gem5.sh
+++ script/gem5.sh
@@ -1,7 +1,6 @@
 #! /usr/bin/env bash
 
-docker compose exec openperf1 $SCRIPT_HOME/gem5_fix.sh # 为xiangshan连上RTC
+docker compose exec openperf1 script/gem5_fix.sh # 为xiangshan连上RTC
 
-cd $GEM5 || exit
-bash ./init.sh
-docker compose exec openperf1 scons build/RISCV/gem5.opt --gold-linker -j "$(nproc)"
+docker compose exec openperf1 bash -c "cd GEM5 && bash ./init.sh"
+docker compose exec openperf1 bash -c "cd GEM5 && scons build/RISCV/gem5.opt --gold-linker -j "$(nproc)""
